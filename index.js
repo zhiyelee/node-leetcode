3950e716763e55e2ec12d21f8ba28cb31c8ba4b6
zhiyelee <zhiyelee@gmail.com>
2015-10-25 20:38:58 -0700

    init

---
+++ index.js
@@ -0,0 +1,72 @@
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+function createList(arr) {
+  var head = new ListNode();
+  var current = head;
+  arr.forEach(function (item) {
+    current.next = new ListNode(item);
+    current = current.next;
+  });
+
+  return head.next;
+}
+
+
+function toArray(node) {
+  var ans = [];
+  while(node) {
+    ans.push(node.val)
+    node = node.next;
+  }
+
+  return ans;
+}
+
+exports.nodeList = {
+  create: createList,
+  toArray: toArray
+};
+
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+// create Tree from array
+// https://leetcode.com/faq/#binary-tree
+function createTree(arr) {
+  if (!arr.length || arr[0] === null) return null;
+  var len = arr.length;
+
+  var res = new TreeNode(arr[0]);
+  var nodes = arr.slice(1);
+  var children = [res];
+  var current = res;
+  while(children.length) {
+    current = children.shift();
+
+    if (!current || current.val === null) {
+      continue;
+    }
+
+    if (nodes.length) {
+      current.left = new TreeNode(nodes.shift());
+      children.push(current.left);
+    }
+
+    if (nodes.length) {
+      current.right = new TreeNode(nodes.shift())
+      children.push(current.right);
+    }
+  }
+
+  return res;
+}
+
+exports.tree = {
+  create: createTree
+};
